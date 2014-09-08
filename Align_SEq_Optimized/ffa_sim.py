841cb023960f5589c0119961303fbc04ea3e4477
craigbenko <craigbenko@gmail.com>
2014-09-08 15:25:24 -0600

    Update
    Added save and load functions.

--- Align_SEq_Optimized/ffa_sim.py
+++ Align_SEq_Optimized/ffa_sim.py
@@ -1,11 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.integrate import ode
 import time
 
 # import my classes
 
-#cimport numpy as np
+# cimport numpy as np
 from laser import *
 from molecule import *
 from integrator import *
@@ -17,15 +15,17 @@ from expectation_values import *
 
 
 class ffa_sim:
+
     '''
     simulator of field free molecular alignment.  Needs molecule [str], temperature [K], pulse width [s] and intensity[10**14 W cm**-2].
     Default values are provided so you can simply run_sim after creating an instance of the ffa_sim class.
     '''
-    def __init__(self, molecule= 'N2', temperature = 100, pulse = 100e-15, intensity = .1):
-            self.molecule = molecule
-            self.temperature = temperature
-            self.pulse = pulse
-            self.intensity = intensity
+
+    def __init__(self, molecule='N2', temperature=100, pulse=100e-15, intensity=.1):
+        self.molecule = molecule
+        self.temperature = temperature
+        self.pulse = pulse
+        self.intensity = intensity
 
     def run_sim(self):
         # start time
@@ -42,16 +42,16 @@ class ffa_sim:
         D = mol.params()[1]
         Jweight = mol.Boltzmann()
 
-        sigma = las.pulse_FWHM*B/hbar
-        E0 = 2.74*10**10*las.Int**.5 # electric field amplitude
-        strength=0.5*4*np.pi*epsilon0*delta_alpha*E0**2/B
+        sigma = las.pulse_FWHM * B / hbar
+        E0 = 2.74 * 10 ** 10 * las.Int ** .5  # electric field amplitude
+        strength = 0.5 * 4 * np.pi * epsilon0 * delta_alpha * E0 ** 2 / B
 
         # create integrator
-        intSEq = integrator(mol.params()[3], las.pulse_FWHM*B/hbar, strength, B, D)
+        intSEq = integrator(
+            mol.params()[3], las.pulse_FWHM * B / hbar, strength, B, D)
 
         # integrate
         Cstor = intSEq.integrate()
-
 
         # Expectation value, incoherent, thermal average.
         exp_val = expectation_values(Jmax, sigma, strength, B, D)
@@ -60,13 +60,18 @@ class ffa_sim:
         elapsed = time.time() - timer
         print('\n Program took ' + str(round(elapsed)) + ' s to run. \n')
         # plot
-        exp_val.cos2plot(tt,cos2, mol.mol_str)
+        exp_val.cos2plot(tt, cos2, mol.mol_str)
 
-        ## End program
+        # End program
         return tt, cos2
 
-    def save_sim(self,tt = 0,cos2 = 0):
+    def save_sim(self, filename = 'dummy.txt', tt=0, cos2=0):
         '''
-        will put output saver here later.
+        Save Data 
         '''
-        print('Not implemented yet!')
+        np.savetxt(filename, np.column_stack((tt,cos2)), delimiter = ',')
+        print('Saved File: ' + filename)
+    
+    def load_sim(self, filename = 'dummy.txt'):
+        return np.genfromtxt(filename, delimiter = ',', dtype = 'complex64') 
+
